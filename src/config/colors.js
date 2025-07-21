253bf02db343dcac488ba42acb7fd6017f054a9b
Higor <higorribeiroaraujo007@gmail.com>
2025-07-20 23:30:50 -0300

    feat: construção dos components e do styled

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#C3073F';
+export const primaryDarkColor = '#1A1A1D';
+
+export const successColor = '#1097f6';
+export const infoColor = '#0197f6';
+export const errorColor = '#f2af29';
+export const warnColor = '#f2af29';
